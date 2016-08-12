b6b91892e29fd2203a0c4e4d1763dd244324ba25
Justin Lazarski <blackhawks189@gmail.com>
2016-08-12 15:09:56 -0500

    Initial Commit

---
+++ simpleCalculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  simpleCalculator
+//
+//  Created by Justin Lazarski on 8/12/16.
+//  Copyright © 2016 Justin Lazarski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
