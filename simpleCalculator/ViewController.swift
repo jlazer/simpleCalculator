770d116ec39606a3d3b652a889a2520b30aef672
Justin Lazarski <blackhawks189@gmail.com>
2016-08-13 01:25:28 -0500

    1st commit. The app is working

--- simpleCalculator/ViewController.swift
+++ simpleCalculator/ViewController.swift
@@ -9,19 +9,56 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    //  Connecting the IBOutlets.
+    @IBOutlet weak var number1TextField: NSTextField!
+    @IBOutlet weak var number2TextField: NSTextField!
+    @IBOutlet weak var productLabel: NSTextField!
+    @IBOutlet weak var operationPopupButton: NSPopUpButton!
+    let operationsArray = ["Add", "Subtract", "Multiply", "Divide"]
+    var selectedItem = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //      Removing the default items and placing the operations array  into the popup button.
+        operationPopupButton.removeAllItems()
+        operationPopupButton.addItemsWithTitles(operationsArray)
+        operationPopupButton.selectItemAtIndex(0)
     }
-
+    @IBAction func calculateOnTap(sender: NSButton) {
+        
+        //      Setting the apropriate textfields to the apropirate variable
+        let number1 = Float(number1TextField.floatValue)
+        let number2 = Float(number2TextField.floatValue)
+        //      Creating the variable outcome
+        var outcome = Float()
+        //      Selected item is now equal to the currently selected title from the popup button
+        selectedItem = operationPopupButton.selectedItem!.title
+        print(number1, number2)
+        print(selectedItem)
+        
+        if selectedItem == "Add"
+        {
+            outcome = Float(number1 + number2)
+        }
+        else if selectedItem == "Subtract"
+        {
+            outcome = Float(number1 - number2)
+        }
+        else if selectedItem == "Multiply"
+        {
+            outcome = Float(number1 * number2)
+        }
+        else if selectedItem == "Divide"
+        {
+            outcome = Float(number1 / number2)
+        }
+        //      Displaying the outcome inside the productLabel Textfield
+        productLabel.floatValue = outcome
+        print(outcome)
+    }
     override var representedObject: AnyObject? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
-
 }
-
