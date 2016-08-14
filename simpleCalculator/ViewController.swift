8613cf01e5d37426e727ceed565bd2539a1e69bc
Justin Lazarski <blackhawks189@gmail.com>
2016-08-14 16:12:26 -0500

    Changed to all numbers to double values to correct a rounding issue.

--- simpleCalculator/ViewController.swift
+++ simpleCalculator/ViewController.swift
@@ -27,10 +27,10 @@ class ViewController: NSViewController {
     @IBAction func calculateOnTap(sender: NSButton) {
         
         //      Setting the apropriate textfields to the apropirate variable
-        let number1 = Float(number1TextField.floatValue)
-        let number2 = Float(number2TextField.floatValue)
+        let number1 = Double(number1TextField.doubleValue)
+        let number2 = Double(number2TextField.doubleValue)
         //      Creating the variable outcome
-        var outcome = Float()
+        var outcome = Double()
         //      Selected item is now equal to the currently selected title from the popup button
         selectedItem = operationPopupButton.selectedItem!.title
         print(number1, number2)
@@ -38,22 +38,27 @@ class ViewController: NSViewController {
         
         if selectedItem == "Add"
         {
-            outcome = Float(number1 + number2)
+            outcome = Double(number1 + number2)
         }
         else if selectedItem == "Subtract"
         {
-            outcome = Float(number1 - number2)
+            outcome = Double(number1 - number2)
         }
         else if selectedItem == "Multiply"
         {
-            outcome = Float(number1 * number2)
+            outcome = Double(number1 * number2)
         }
         else if selectedItem == "Divide"
         {
-            outcome = Float(number1 / number2)
+            outcome = Double(number1 / number2)
         }
+        func roundToPlaces(value: Double, decimalPlaces: Int) -> Double {
+            let divisor = pow(10.0, Double(decimalPlaces))
+            return round(value * divisor) / divisor
+        }
+        
         //      Displaying the outcome inside the productLabel Textfield
-        productLabel.floatValue = outcome
+        productLabel.doubleValue = outcome
         print(outcome)
     }
     override var representedObject: AnyObject? {
